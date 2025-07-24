8af3f047eeb171e0c9cd8adba6d0d1ff9799c46b
Ramesh <ramesh200212@gmail.com>
2025-07-24 20:53:59 +0530

    Print and Variables in python

---
+++ VariablePython.py
@@ -0,0 +1,17 @@
+character_name = "John"
+character_age = "70"
+
+print("Once there was a man named " + character_name + ".")
+print("He was " + character_age +" years old.")
+
+character_name = "Mike"
+print("He really liked the name "+ character_name + ".")
+print("But he didn't like being 70.")
+
+
+# This code demonstrates the use of variables in Python.
+# Variables can hold different types of data, such as strings and integers.
+# Variables can be reassigned to new values.
+# The code prints out a story using the variables defined above.
+# Variables can be used to store information that can be reused later in the code.
+# Variables can be used to make code more readable and maintainable.
