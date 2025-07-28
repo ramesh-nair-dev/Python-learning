4d0d437385b766ec64bfde214566b4581c9f3d06
Ramesh <ramesh200212@gmail.com>
2025-07-28 12:36:26 +0530

    Intro to list and basic functionality

---
+++ BasicIntroToList.py
@@ -0,0 +1,33 @@
+friends = ["Jon Snow","Arthur Morgan","Kratos","Robb Stark","Arya Stark"]
+
+print(friends)
+# This code demonstrates how to create a list of friends in Python.
+
+print(friends[0])
+# This code demonstrates how to access the first element of a list in Python.
+
+print(friends[1])
+# This code demonstrates how to access the second element of a list in Python.
+
+print(friends[2])
+# This code demonstrates how to access the third element of a list in Python.
+
+friends[4] = "Ragnar Lothbrok"
+print(friends)
+# This code demonstrates how to modify an element in a list in Python.
+
+print(friends[-1])
+# This code demonstrates how to access the last element of a list in Python using negative indexing.
+
+
+print(friends[-2])
+# This code demonstrates how to access the second-to-last element of a list in Python using negative indexing.
+
+print(friends[1:4])
+# This code demonstrates how to access a slice of a list in Python.
+
+print(friends[1:])
+# This code demonstrates how to access a slice of a list in Python, starting from the second element to the end.
+
+print(friends[:3])
+# This code demonstrates how to access a slice of a list in Python, from the beginning up to the third element (exclusive).
