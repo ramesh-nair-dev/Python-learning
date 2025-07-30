ec57df4a7586c985b4c1505130b6fb2dd0e8c954
Ramesh <ramesh200212@gmail.com>
2025-07-30 10:23:37 +0530

    Understanding Dictionaries

---
+++ Practice/Dictionaries.py
@@ -0,0 +1,19 @@
+monthCovertion = {
+    "Jan": "January",
+    "Feb": "February",
+    "Mar": "March",
+    "Apr": "April",
+    "May": "May",
+    "Jun": "June",
+    "Jul": "July",
+    "Aug": "August",
+    "Sep": "September",
+    "Oct": "October",
+    "Nov": "November",
+    "Dec": "December"
+}
+
+print(monthCovertion["Jan"])
+
+
+
