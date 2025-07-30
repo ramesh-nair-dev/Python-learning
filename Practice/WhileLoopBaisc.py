e6de39fbbd6c25beced1ae15c06c89249162b724
Ramesh <ramesh200212@gmail.com>
2025-07-30 10:28:12 +0530

    While loop basic

---
+++ Practice/WhileLoopBaisc.py
@@ -0,0 +1,15 @@
+num = 1
+
+while num <= 10:
+    print(num)
+    num += 1
+
+print("Loop finished")
+
+# This code initializes a variable `num` to 1 and enters a while loop that continues as long as `num` is less than or equal to 10.
+# Inside the loop, it prints the current value of `num` and then increments `num` by 1.
+# When `num` exceeds 10, the loop terminates and "Loop finished" is printed.
+# The loop will print numbers from 1 to 10, each on a new line.
+# This is a basic example of a while loop in Python.
+# The loop will print numbers from 1 to 10, each on a new line.
+
