c6587a7ec2c65c908421837aa4fe579b96d8ef4e
Ramesh <ramesh200212@gmail.com>
2025-07-30 13:03:32 +0530

    classes in python

---
+++ Practice/OOPS/Client.py
@@ -0,0 +1,17 @@
+from Student import Student
+
+Student1 = Student("John", 20, 101)
+print(Student1.name)
+print(Student1.age)
+print(Student1.roll_no)
+
+Student2 = Student("Alice", 22, 102)
+print(Student2.name)
+print(Student2.age)
+print(Student2.roll_no)
+
+# We are creating two instances of the Student class, Student1 and Student2.
+# Each instance has its own attributes: name, age, and roll_no.
+# The code then prints the attributes of each student instance.
+# This demonstrates how to create and use objects in Python using a class.
+# The output will show the name, age, and roll number of each student.
---
+++ Practice/OOPS/Student.py
@@ -0,0 +1,10 @@
+class Student:
+    def __init__(self, name,age,roll_no):
+        self.name = name
+        self.age = age
+        self.roll_no = roll_no
+
+# The __init__ method is a special method in Python classes that initializes the object's attributes.
+# It is called when an object of the class is created.
+# In this case, it initializes the attributes name, age, and roll_no for each Student object.
+# The self parameter refers to the instance of the class itself, allowing access to its attributes and methods.
