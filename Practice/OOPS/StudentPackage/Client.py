4d035d783c85cb1f517b929b9fffb8c9d3e9d183
Ramesh <ramesh200212@gmail.com>
2025-07-30 13:41:14 +0530

    Added object level methods in Student class

--- Practice/OOPS/StudentPackage/Client.py
+++ Practice/OOPS/StudentPackage/Client.py
@@ -4,11 +4,13 @@ Student1 = Student("John", 20, 101)
 print(Student1.name)
 print(Student1.age)
 print(Student1.roll_no)
+print(Student1.displayStudent())
 
 Student2 = Student("Alice", 22, 102)
 print(Student2.name)
 print(Student2.age)
 print(Student2.roll_no)
+print(Student2.displayStudent())
 
 # We are creating two instances of the Student class, Student1 and Student2.
 # Each instance has its own attributes: name, age, and roll_no.
--- Practice/OOPS/StudentPackage/Student.py
+++ Practice/OOPS/StudentPackage/Student.py
@@ -3,6 +3,10 @@ class Student:
         self.name = name
         self.age = age
         self.roll_no = roll_no
+    
+    def displayStudent(self):
+        print(f"Name: {self.name}, Age: {self.age}, Roll No: {self.roll_no}") # -- This object level method
+    
 
 # The __init__ method is a special method in Python classes that initializes the object's attributes.
 # It is called when an object of the class is created.
